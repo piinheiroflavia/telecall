a05c8d17fb7af55f2bc67566d4487a94a3b382df
Flavia Pinheio <flavia.p_17@hotmail.com>
2022-05-11 13:42:57 -0300

    iniciando tela de cadastro

--- script/script.js
+++ script/script.js
@@ -5,7 +5,63 @@ $('#cpfcnpj').mask('000.000.000-00', {
       const mask = (cpfcnpj.length > 14) ? masks[1] : masks[0];
       $('#cpfcnpj').mask(mask, options);
     }
-  });
+});
+
+//function feedback(){
+// 
+//}
+
+function validar(){
+	var nome = document.getElementById('#nome')
+	var social = document.getElementById("#nome-social");
+	var nascimento = document.getElementById("#data-nascimento");
+	var sexo = document.getElementById("#sexo");
+	var senha= document.getElementById("#senha");
+	var confirma= document.getElementById("#confirma");
+	var res = document.getElementById('#res');
+
+   alert('ola')
+	if(nome.value==''){
+		res.innerHTML='Mensagem de Erro:O campo nome é obrigatório';
+	
+	
+	}
+	
+	else if(social.value==''){
+    res.innerHTML='Mensagem de Erro:O campo endereço é obrigatório';
+	
+	}
+	
+	else if(nascimento.value=='0'){
+    res.innerHTML= 'Mensagem de Erro:O campo formação é obrigatório';
+	
+	
+	}
+	
+	else if(sexo.value==''){
+    res.innerHTML= 'Mensagem de Erro:O campo login é obrigatório';
+	
+		
+	}
+	
+	else if(senha.value==''){
+    res.innerHTML= 'Mensagem de Erro:O campo senha é obrigatório';
+	
+	
+	}
+	
+	else if(senha.value!=confirma.value){
+		res.innerHTML='Mensagem de Erro:Os campos senha e confirma senha devem ser iguais';
+	
+
+	}
+	else {
+		document.formulario.submit();
+	}
+	
+}
+
+
 
 //OCULTAR A SENHA
 function showPassword() {
