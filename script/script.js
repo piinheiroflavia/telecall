d97032e0b6da18e1f81a20b26f4fbd070f05cd9e
Flavia Pinheio <flavia.p_17@hotmail.com>
2022-05-12 22:23:42 -0300

    responsividade na tela cliente e cadastro

--- script/script.js
+++ script/script.js
@@ -6,77 +6,109 @@ $('#cpfcnpj').mask('000.000.000-00', {
       $('#cpfcnpj').mask(mask, options);
     }
 });
+/*TELEFONE*/
+$("#telefone")
+        .mask("(+99) 99 9999-9999")
+        .focusout(function (event) {  
+            var target, phone, element;  
+            target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
+            phone = target.value.replace(/\D/g, '');
+            element = $(target);  
+            element.unmask();  
+            if(phone.length > 12) {  
+                element.mask("(+99) 99 9999-9999");  
+            } else {  
+                element.mask("(+99) 99 9999-9999");  
+            }  
+        });
+/*CELULAR*/
+$("#celular")
+        .mask("(+99) 99 9 9999-9999")
+        .focusout(function (event) {  
+            var target, phone, element;  
+            target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
+            phone = target.value.replace(/\D/g, '');
+            element = $(target);  
+            element.unmask();  
+            if(phone.length > 12) {  
+                element.mask("(+99) 99 9 9999-9999");  
+            } else {  
+                element.mask("(+99) 99 99999-9999");  
+            }  
+        });
+/*EMAIL*/
+$(document).ready(function() {
+  $("#nascimento")
+    .mask("99/99/9999",{placeholder:"mm/dd/yyyy"})
+});
 
-//function feedback(){
-// 
-//}
+
+
+
+	let senha = document.getElementById("Senha");
+	let confirma = document.getElementById("confirmar");
 
 function validar(){
-	var nome = document.getElementById('#nome')
-	var social = document.getElementById("#nome-social");
-	var nascimento = document.getElementById("#data-nascimento");
-	var sexo = document.getElementById("#sexo");
-	var senha= document.getElementById("#senha");
-	var confirma= document.getElementById("#confirma");
-	var res = document.getElementById('#res');
+	let nome = document.getElementById('nome')
+	let nascimento = document.getElementById("nascimento");
+	let email = document.getElementById("nomeMaterno");
+	let materno = document.getElementById("email");
+	let endereco = document.getElementById("endereco");
+	let cpfcnpj = document.getElementById("cpfcnpj");
+	let telefone = document.getElementById("telefone");
+	let celular = document.getElementById("celular");
+	var res = document.getElementById('res');
 
-   alert('ola')
-	if(nome.value==''){
-		res.innerHTML='Mensagem de Erro:O campo nome é obrigatório';
-	
-	
-	}
-	
-	else if(social.value==''){
-    res.innerHTML='Mensagem de Erro:O campo endereço é obrigatório';
-	
-	}
-	
-	else if(nascimento.value=='0'){
-    res.innerHTML= 'Mensagem de Erro:O campo formação é obrigatório';
-	
-	
-	}
-	
-	else if(sexo.value==''){
-    res.innerHTML= 'Mensagem de Erro:O campo login é obrigatório';
-	
-		
-	}
-	
-	else if(senha.value==''){
-    res.innerHTML= 'Mensagem de Erro:O campo senha é obrigatório';
-	
-	
-	}
-	
-	else if(senha.value!=confirma.value){
-		res.innerHTML='Mensagem de Erro:Os campos senha e confirma senha devem ser iguais';
-	
+	if(nome.value == ''){
+		res.innerHTML+='Mensagem de Erro: O campo NOME é obrigatório';		
 
-	}
-	else {
-		document.formulario.submit();
+	}else if(nascimento.value==''){
+    	res.innerHTML = 'Mensagem de Erro:O campo DATA DE NASCIMENTO é obrigatório';
+
+	}else if(materno.value==''){
+    	res.innerHTML ='Mensagem de Erro: O campo NOME MATERNO é obrigatório';
+
+	}else if(email.value==''){
+    	res.innerHTML ='Mensagem de Erro: O campo EMAIL é obrigatório';	
+
+	}else if(endereco.value==''){
+    	res.innerHTML ='Mensagem de Erro: O campo ENDEREÇO é obrigatório';
+
+	}else if(cpfcnpj.value==''){
+    	res.innerHTML ='Mensagem de Erro: O campo CPF ou CNPJ é obrigatório';
+	
+  }else if(celular.value==''){
+		res.innerHTML ='Mensagem de Erro: O campo CELULAR é obrigatório';
+	
+  }else if(telefone.value==''){
+    	res.innerHTML ='Mensagem de Erro: O campo TELEFONE FIXO é obrigatório';
+
+	}else if(senha.value==''){
+    	res.innerHTML = 'Mensagem de Erro: O campo SENHA é obrigatório';
+
+	}else if(confirma.value==''){
+    	res.innerHTML = 'Mensagem de Erro: O campo CONFIRMAR SENHA é obrigatório';
+
+	}else if(senha.value != confirma.value){
+		res.innerHTML ='Mensagem de Erro: Os campos SENHA e CONFIRMAR SENHA devem ser iguais';
+	}else{
+	  document.formulario.submit();
 	}
 	
 }
 
-
-
 //OCULTAR A SENHA
 function showPassword() {
-  const senha = document.getElementById('Senha');
   const aberto = document.getElementById('lock');
   const fechado = document.getElementById('unlock')
 
   if (aberto.style.display === 'block'){
     aberto.style.display = 'none';
     fechado.style.display = 'block';
-    senha.type = 'text';
+    senha.confirma.type = 'text';
   }else{
     aberto.style.display = 'block';
     fechado.style.display = 'none';
-    senha.type = 'password';
+    senha.confirma.type = 'password';
   }
-};
-
+}
